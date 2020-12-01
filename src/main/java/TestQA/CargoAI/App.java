cc49ffaaab5387efa741164ff1048193e733c3a1
Marina <marina@wedoqa.com>
2020-12-01 10:00:07 +0100

    New files for tracking

---
+++ src/main/java/TestQA/CargoAI/App.java
@@ -0,0 +1,13 @@
+package TestQA.CargoAI;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
